05418e6e2cb3f3e4ed44455c6db09d65988636ec
ksenialearn <ksenia.sayenko@gmail.com>
2015-07-16 20:27:56 -0400

    Create removeVowels.py

---
+++ removeVowels.py
@@ -0,0 +1,15 @@
+#Function removeVowels() 
+#search for each vowel (a,e,i,o,u) in the string 
+
+def removeVowels(st):
+	l= len(st)
+	k= 0
+	st1= ""
+	while k<l:
+		if st[k]== 'a' or st[k]== 'i' or st[k]== 'o' or st[k]== 'u' or st[k]== 'e':
+			st1= st1
+			k= k+1
+		else:
+			st1= st1 + st[k]
+			k= k+1
+	print(st1)
